c365931115ee4fbfbd0bc1ebe77e42d2f2b75ac4
dilshan <rajapaksharld@gmail.com>
2023-06-09 12:41:34 +0530

    plot3

---
+++ Linux/plot3.sh
@@ -0,0 +1,175 @@
+#!/bin/bash
+
+
+mkdir data_para hours para logs output_dir para
+
+log_files=("NgwStats.9" "NgwStats.8" "NgwStats.7" "NgwStats.6" "NgwStats.5" "NgwStats.4" "NgwStats.3" "NgwStats.2" "NgwStats.1" "NgwStats.0")
+
+
+
+for file in "${log_files[@]}"; do 
+    cat /opt/aepona/ngw/logs/"$file" >> /opt/aepona/ngw/gwuser/logs/combined_log
+done
+
+
+
+#cat /home/dilshan/Desktop/ngw/logs/* > /opt/aepona/ngw/gwuser/logs/combined_log
+
+
+#sort -k1,1 -k2,2 -k3,3 -o "/opt/aepona/ngw/gwuser/logs/sorted_log_file" "/opt/aepona/ngw/gwuser/logs/combined_log"
+
+awk -F " " '{print $1" "$2" "$3"\t"$9" "$10" "$11" "$12" "$13" "$14" "$15}' /opt/aepona/ngw/gwuser/logs/combined_log >test1
+
+awk -F'[: ]' '{ date=$1" "$2; sub(/:/, "", date); file=sprintf("%s/log_%s.log", "'output_dir'", date); print >> file }' "/opt/aepona/ngw/gwuser/test1"
+
+
+
+count=( "sendSms" "sendSmsError" "notifySmsReceptionFailure" "notifySmsDeliveryReceiptFailure" "notifySmsReception" "notifySmsDeliveryReceipt" "sendSmsResponse" "sendSmsResponseFailure" "GenericNack IN" "GenericNack OUT" "BindTX" "BindTX Resp" "Unbind" "Unbind Resp" "SubmitSM" "SubmitSM Resp" "SubmitSmMulti" "SubmitSmMultiResp" "Enquire Link PDUs sent" "Enquire Link Rsp PDUs received" "Enquire Link PDUs recevied" "Enquire Link Rsp PDUs sent" "DeliverSM" "DeliverSMResp" )
+
+
+for file in /opt/aepona/ngw/gwuser/output_dir/*; do
+
+
+date1=$( awk 'NR==1 {print $1, $2; exit}' "$file" )
+
+
+
+sed -n "/00:00:00/,/01:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour1
+sed -n "/01:00:00/,/02:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour2
+sed -n "/02:00:00/,/03:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour3
+sed -n "/03:00:00/,/04:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour4
+sed -n "/04:00:00/,/05:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour5 
+sed -n "/05:00:00/,/06:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour6
+sed -n "/06:00:00/,/07:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour7
+sed -n "/07:00:00/,/08:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour8
+sed -n "/08:00:00/,/09:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour9
+sed -n "/09:00:00/,/10:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour10
+sed -n "/10:00:00/,/11:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour11
+sed -n "/11:00:00/,/12:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour12
+sed -n "/12:00:00/,/13:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour13
+sed -n "/13:00:00/,/14:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour14
+sed -n "/14:00:00/,/15:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour15
+sed -n "/15:00:00/,/16:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour16
+sed -n "/16:00:00/,/17:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour17
+sed -n "/17:00:00/,/18:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour18
+sed -n "/18:00:00/,/19:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour19
+sed -n "/19:00:00/,/20:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour20
+sed -n "/20:00:00/,/21:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour21
+sed -n "/21:00:00/,/22:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour22
+sed -n "/22:00:00/,/23:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour23
+sed -n "/23:00:00/,/00:00:00/p" "$file" >/opt/aepona/ngw/gwuser/hours/hour24
+
+
+
+
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour1
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour2
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour3
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour4
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour5
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour6
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour7
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour8
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour9
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour10
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour11
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour12
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour13
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour14
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour15
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour16
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour17
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour18
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour19
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour20
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour21
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour22
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour23
+sed -i '$ d' /opt/aepona/ngw/gwuser/hours/hour24
+
+
+yellow='\033[0;33m'
+    reset='\033[0m'
+    echo -e "${yellow}****************************************************************** Date = $date1 *************************************************************************${reset}"
+
+#echo "///////////////////////////////// Date = $file /////////////////////////////////////////"
+
+
+for word in "${count[@]}"; do
+
+
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour1 | awk -F"=" '{sum += $2} END {print "00:00-01:00   " sum}' >/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour2 | awk -F"=" '{sum += $2} END {print "01:00-02:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour3 | awk -F"=" '{sum += $2} END {print "02:00-03:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour4 | awk -F"=" '{sum += $2} END {print "03:00-04:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour5 | awk -F"=" '{sum += $2} END {print "04:00-05:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour6 | awk -F"=" '{sum += $2} END {print "05:00-06:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour7 | awk -F"=" '{sum += $2} END {print "06:00-07:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour8 | awk -F"=" '{sum += $2} END {print "07:00-08:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour9 | awk -F"=" '{sum += $2} END {print "08:00-09:00   " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour10 | awk -F"=" '{sum += $2} END {print "09:00-10:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour11 | awk -F"=" '{sum += $2} END {print "10:00-11:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour12 | awk -F"=" '{sum += $2} END {print "11:00-12:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour13 | awk -F"=" '{sum += $2} END {print "12:00-13:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour14 | awk -F"=" '{sum += $2} END {print "13:00-14:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour15 | awk -F"=" '{sum += $2} END {print "14:00-15:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour16 | awk -F"=" '{sum += $2} END {print "15:00-16:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour17 | awk -F"=" '{sum += $2} END {print "16:00-17:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour18 | awk -F"=" '{sum += $2} END {print "17:00-18:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour19 | awk -F"=" '{sum += $2} END {print "18:00-19:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour20 | awk -F"=" '{sum += $2} END {print "19:00-20:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour21 | awk -F"=" '{sum += $2} END {print "20:00-21:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour22 | awk -F"=" '{sum += $2} END {print "21:00-22:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour23 | awk -F"=" '{sum += $2} END {print "22:00-23:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+    grep "$word" /opt/aepona/ngw/gwuser/hours/hour24 | awk -F"=" '{sum += $2} END {print "23:00-24:00  " sum}' >>/opt/aepona/ngw/gwuser/data_para/data"$word".txt
+
+
+
+    awk 'NF > 1' /opt/aepona/ngw/gwuser/data_para/data"$word".txt > /opt/aepona/ngw/gwuser/para/"$word".txt 
+
+
+
+    
+    echo "$date1 $word"
+
+
+    # Read the data from the file
+data_file=/opt/aepona/ngw/gwuser/para/"$word".txt
+data=$(cat "$data_file")
+
+# Iterate over each line of the data
+while IFS= read -r line; do
+  time=$(echo "$line" | awk '{print $1}')
+  value=$(echo "$line" | awk '{print $2}')
+
+
+
+  stars=$(( value / 100000 ))
+
+  # Print the time and asterisks
+  echo -n "$time: "
+  for ((i=1; i<=stars; i++)); do
+    echo -n "▀"
+  done
+
+  # Print the value
+  echo " ($value)"
+done <<< "$data"
+
+
+
+
+echo "                      " 
+
+
+    #termgraph < /opt/aepona/ngw/gwuser/para/"$word".txt
+    
+
+done
+
+
+
+done
+
+
+
